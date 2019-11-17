4296d6ab4031edb099c825728625032b7a265f35
Ascanio Savio <ascaniosavio97@gmail.com>
2019-11-17 13:31:18 -0300

    Create question1e2.pl

---
+++ question1e2.pl
@@ -0,0 +1,41 @@
+passou(pedro, calculo1).
+passou(joao, calculo1).
+passou(fabio, calculo1).
+passou(paulo, calculo1).
+
+passou(pedro, programacao1).
+passou(joao, programacao1).
+passou(fabio, programacao1).
+passou(paulo, programacao1).
+
+passou(ana, fisica1).
+passou(maria, fisica1).
+passou(emanuela, fisica1).
+passou(fabiola, fisica1).
+
+passou(ana, inglesinstrumental).
+passou(maria, inglesinstrumental).
+passou(emanuela, inglesinstrumental).
+passou(fabiola, inglesinstrumental).
+
+passou(ana, algebra).
+passou(pedro, algebra).
+passou(joao, algebra).
+passou(maria, algebra).
+passou(fabio, algebra).
+passou(paulo, algebra).
+
+passou(emanuela, informatica).
+passou(fabiola, informatica).
+passou(joao, informatica).
+passou(pedro, informatica).
+
+passou(emanuela, sociedade).
+passou(fabiola, sociedade).
+passou(joao, sociedade).
+passou(pedro, sociedade).
+
+passouf(P1, P2) :-
+			passou(N1, P1), passou(N1, P2), write(N1), nl, fail. 
+
+?- passouf(programacao1, algebra)
