25149c5849cb3c262f6a61def39139e33a5303ea
Ascanio Savio <ascaniosavio97@gmail.com>
2019-11-17 13:35:06 -0300

    Update question4.pl

--- question4.pl
+++ question4.pl
@@ -1,10 +1,10 @@
 pg(0, T, R, S) :- write(S), nl.
 pg(N, T, R, S) :-
 	 
-			S1 is S + T,
-			T1 is T * R,
-			N1 is N - 1,
-				
-			pg(N1, T1, R, S1).
+		S1 is S + T,
+		T1 is T * R,
+		N1 is N - 1,
+		pg(N1, T1, R, S1).
+		
 %foi colocado o 3 no lugar do 0 para evitar incongruências
 ?- pg(3, 5, 2, 0).
