5819203f63a17c3d102328b59ac4413da4044dc5
Ascanio Savio <ascaniosavio97@gmail.com>
2019-11-17 13:34:30 -0300

    Update question5.pl

--- question5.pl
+++ question5.pl
@@ -1,8 +1,8 @@
 desenhar(1, _) :- write("XX X").
 desenhar(H, B) :- 
-				write("X XX"),
-				nl,
-				H1 is H - 1,
-				desenhar(H1, B).			
+		write("X XX"),
+		nl,
+		H1 is H - 1,
+		desenhar(H1, B).			
 			
 ?- desenhar(2,4).
